795e4a9cc1f0ee15b72fbaffc94f98e08e129852
porkrindplz <68478716+porkrindplz@users.noreply.github.com>
2024-09-14 18:35:54 -0400

    leaderboards!

---
+++ Assets/_Scripts/Network/Leaderboard.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using __Scripts.Systems;
+using LootLocker.Requests;
+using UnityEngine;
+using Logger = _Scripts.Utilities.Logger;
+using TMPro;
+using UnityEngine.UI;
+
+namespace _Scripts.Network
+{
+    public class Leaderboard : MonoBehaviour
+    {
+        private string leaderboardID = "acornsAtEnding";
+ 
+        [SerializeField] TextMeshProUGUI playerNames;
+        [SerializeField] TextMeshProUGUI playerScores;
+        [SerializeField] private TextMeshProUGUI currentPlayerPlacement;
+ 
+        public IEnumerator SubmitScoreRoutine(int score)
+        {
+            bool done = false;
+            string playerID = PlayerPrefs.GetString("PlayerID");
+            LootLockerSDKManager.SubmitScore(playerID,score,leaderboardID, (response) =>
+            {
+                if (response.success)
+                {
+                    Logger.Log("Score submitted");
+                    done = true;
+                }
+                else
+                {
+                    Logger.Log($"Failed. + {response.errorData}");
+                    done = true;
+                }
+            });
+            yield return new WaitWhile(() => done == false);
+        }
+
+        public IEnumerator FetchHighScoresRoutine()
+        {
+            bool done = false;
+            LootLockerSDKManager.GetScoreList(leaderboardID, 10, 0, (response) =>
+            {
+                if (response.success)
+                {
+                    string tempPlayerNames = "Names\n";
+                    string tempPlayerScores = "Scores\n";
+
+                    LootLockerLeaderboardMember[] members = response.items;
+                    for (int i = 0; i < members.Length; i++)
+                    {
+                        if (members[i].player.name != "")
+                        {
+                            tempPlayerNames += members[i].rank + ". ";
+                        }
+                        else
+                        {
+                            tempPlayerNames += members[i].player.id;
+                        }
+
+                        tempPlayerScores += members[i].score + " \n";
+                        tempPlayerNames += "\n";
+                    }
+
+                    done = true;
+                    playerNames.text = tempPlayerNames;
+                    playerScores.text = tempPlayerScores;
+                }
+                else
+                {
+                    Logger.Log($"Failed. + {response.errorData}");
+                    done = true;
+                }
+
+            });
+            yield return new WaitWhile(() => done == false);
+        }
+
+        public IEnumerator FetchCurrentPlayerPlacement()
+        {
+            bool done = false;
+            string playerID = PlayerPrefs.GetString("PlayerID");
+            LootLockerSDKManager.GetMemberRank(leaderboardID, playerID, (response) =>
+            {
+                if (response.success)
+                {
+                    currentPlayerPlacement.text = $"Your top placement is {response.rank.ToString()} with {response.score.ToString()} acorns.";
+                    done = true;
+                }
+                else
+                {
+                    Logger.Log($"Failed. + {response.errorData}");
+                    currentPlayerPlacement.text ="No personal score ranking found.";
+                    done = true;
+                }
+            });
+            yield return new WaitWhile(() => done == false);
+        }
+        public void CloseLeaderboard()
+        {
+            AudioSystem.Instance.PlayMenuSelectSound();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Scripts/Network/OnlinePlayerManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using LootLocker.Requests;
+using UnityEngine;
+using Logger = _Scripts.Utilities.Logger;
+
+namespace _Scripts.Network
+{
+    public class OnlinePlayerManager : MonoBehaviour
+    {
+    
+        public Leaderboard leaderboard;
+        private void Start()
+        {
+            StartCoroutine(SetupRoutine());
+        }
+
+        IEnumerator SetupRoutine()
+        {
+            yield return LoginRoutine();
+            yield return leaderboard.FetchHighScoresRoutine();
+            yield return leaderboard.FetchCurrentPlayerPlacement();
+        }
+
+        IEnumerator LoginRoutine()
+        {
+            bool done = false;
+            LootLockerSDKManager.StartGuestSession((response) =>
+            {
+                if (response.success)
+                {
+                    Logger.Log("Player was logged in");
+                    PlayerPrefs.SetString("PlayerID", response.player_id.ToString());
+                    done = true;
+                }
+                else
+                {
+                    Logger.Log("Could not start session");
+                    done = true;
+                }
+            });
+            yield return new WaitWhile(() => done == false);
+        }
+    }
+}
